143b1dea7cbfaa49ca358e1eb879a1d654d11256
Tayokanch <tayoquadri11@gmail.com>
2026-03-21 22:13:40 +0000

    multiple Inner Join

---
+++ SQL/PostgreSQL/join/multiple_join.sql
@@ -0,0 +1,54 @@
+
+
+-- EXAMPLE 2
+
+SELECT
+    p1.country,
+    p1.continent,
+    president,
+    prime_minister.
+    pm_start
+FROM prime_minister as p1
+INNER JOIN presidents as p2
+USING(country)
+INNER JOIN prime_minister_term as p3 -- The result of the first Inner JOIN with this
+USING(prime_minister);
+
+
+
+-- EXAMPLE 3: using AND to limit the JOIN
+
+SELECT *
+FROM left_table
+INNER JOIN right_table
+ON left_tabl.id = right_table.id
+INNNER JOIN another_table
+ON  left_table.id = another_table.id
+    AND left_table.date = right_table.date ;
+
+--EXAMPLE 4 :
+
+Select c.name, p.fertility_rate
+FROM countries as c
+INNER JOIN populations as p
+ON c.code = p.country_code
+
+--EXAMPLE 5 :
+
+SELECT name, e.year, fertility_rate, e.unemployment_rate
+FROM countries AS c
+INNER JOIN populations AS p
+ON c.code = p.country_code
+-- Join to economies (as e)
+INNER JOIN economies as e
+ON e.code = c.code;
+-- Match on country code
+
+-- Example 6:
+SELECT name, e.year, fertility_rate, unemployment_rate
+FROM countries AS c
+INNER JOIN populations AS p
+ON c.code = p.country_code
+INNER JOIN economies AS e
+ON c.code = e.code
+	AND e.year = p.year
